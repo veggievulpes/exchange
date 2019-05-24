aa24bbb464a6cb78354d03effe63fd10c92242b8
Milton Pereira <milton.pereira@PITSolution.local>
2019-05-24 14:58:37 -0300

    port update on app.js

--- src/app.js
+++ src/app.js
@@ -56,7 +56,8 @@ app.get('*', (req, res) => {
     })
 })
 
+var port = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is online on port 3000')
 })
